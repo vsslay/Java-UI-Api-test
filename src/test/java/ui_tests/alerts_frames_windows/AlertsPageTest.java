d81e5b0fc893c07e5728ca64c6f8fd7a1962cbe3
Andrew Khrystsiuk <andrey.k@okay.qa>
2022-09-29 16:58:49 +0300

    added description for few tests, updated documentation

--- src/test/java/ui_tests/alerts_frames_windows/AlertsPageTest.java
+++ src/test/java/ui_tests/alerts_frames_windows/AlertsPageTest.java
@@ -1,6 +1,9 @@
 package ui_tests.alerts_frames_windows;
 
 import base.BaseTest;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import jdk.jfr.Description;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -13,22 +16,30 @@ public class AlertsPageTest extends BaseTest {
         basePage.open(ALERTS_PAGE);
     }
 
-    @Test
+    @Test(testName = "Accept alert", groups = "Alerts, frames, windows page")
+    @Description("Confirm alert")
+    @Severity(SeverityLevel.NORMAL)
     public void testAlert() {
         alertsPage.clickFirstAlert();
     }
 
-    @Test
+    @Test(testName = "Accept first alert after 5 seconds", groups = "Alerts, frames, windows page")
+    @Description("Confirm alert after wait")
+    @Severity(SeverityLevel.NORMAL)
     public void testAlertAfterFiveSeconds() {
         alertsPage.clickAlertAndWaitToConfirm();
     }
 
-    @Test
+    @Test(testName = "Accept or decline alert", groups = "Alerts, frames, windows page")
+    @Description("Confirm alert from choice")
+    @Severity(SeverityLevel.NORMAL)
     public void testAlertAcceptThanDecline() {
         alertsPage.clickAlertAndConfirm();
     }
 
-    @Test
+    @Test(testName = "Accept alert after sending text", groups = "Alerts, frames, windows page")
+    @Description("Confirm alert with text")
+    @Severity(SeverityLevel.CRITICAL)
     public void testAlertSendTextAndAccept() {
         alertsPage.sendTextToAlert();
     }
--- src/test/java/ui_tests/alerts_frames_windows/BrowserWindowsPageTest.java
+++ src/test/java/ui_tests/alerts_frames_windows/BrowserWindowsPageTest.java
@@ -1,6 +1,9 @@
 package ui_tests.alerts_frames_windows;
 
 import base.BaseTest;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import jdk.jfr.Description;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -13,17 +16,23 @@ public class BrowserWindowsPageTest extends BaseTest {
         basePage.open(BROWSER_WINDOW_PAGE);
     }
 
-    @Test
+    @Test(testName = "Check message", groups = "Alerts, frames, windows page")
+    @Description("Check message")
+    @Severity(SeverityLevel.NORMAL)
     public void openNewTab() {
         browserWindowsPage.openTab();
     }
 
-    @Test
+    @Test(testName = "Open new window", groups = "Alerts, frames, windows page")
+    @Description("Open window")
+    @Severity(SeverityLevel.CRITICAL)
     public void openNewWindow() {
         browserWindowsPage.openWindow();
     }
 
-    @Test
+    @Test(testName = "Open message", groups = "Alerts, frames, windows page")
+    @Description("Open message")
+    @Severity(SeverityLevel.MINOR)
     public void openNewMessage() {
         browserWindowsPage.openMessage();
     }
